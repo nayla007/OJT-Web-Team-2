b727fd550cbd8e6cbe78f37578018faa7af99b2d
atiarart <atiarart@gmail.com>
2025-10-30 14:48:19 +0700

    menambahkan folder pages

---
+++ src/pages/Beranda.jsx
@@ -0,0 +1,9 @@
+export default function Beranda() {
+  return (<div>
+    <h1>ini beranda</h1>
+    <h1>ini beranda</h1>
+    <h1>ini beranda</h1>
+    <h1>ini beranda</h1>
+    <h1>ini beranda</h1>
+    <h1>ini beranda</h1>
+  </div>)}
---
+++ src/pages/Pelatihan.jsx
@@ -0,0 +1,25 @@
+export default function Pelatihan() {
+  return (<div>
+    <h1>ini pelatihan</h1>
+    <h1>ini pelatihan</h1>
+    <h1>ini pelatihan</h1>
+    <h1>ini pelatihan</h1>
+    <h1>ini pelatihan</h1>
+    <h1>ini pelatihan</h1>
+    <p>
+      Internal Auditor SMK3
+      Operator Penggerak Mula Jenis Genset
+      Petugas P3K Ditempat Kerja
+      Teknisi K3 Listrik
+      TKBT Tingkat II
+      Supervisi K3 Perancah
+      Teknisi K3 Deteksi Gas 
+      Teknisi K3 Listrik
+      Teknisi K3 PAA
+      Teknisi K3 Perancah
+      Teknisi K3 PTP
+      Teknisi K3 Tangki Timbun dan Bejana Tekan
+      Teknisi PTP.doc
+      TKBT Level 2
+    </p>
+  </div>)}
